e226634c9e233646a171951bbbff450104ead985
Abupositivity <abupositivity@gmail.com>
2024-01-25 19:05:37 -0800

    monty updates

--- pall.c
+++ pall.c
@@ -1,16 +1,16 @@
 #include "monty.h"
 /**
-* f_fall - prints the stack
+* f_pall - prints the stack
 * @head: stack head
 * @counter: no use
 * Return: no return
 */
-void f_fall(stack_t **head, unsigned int counter)
+void f_pall(stack_t **head, unsigned int counter)
 {
 stack_t *h;
 (void)counter;
 
-h - *head;
+h = *head;
 if (h == NULL)
 return;
 while (h)
